6216f85eed043e4a36a465ea03a93a05f7f85fb2
CHINTAMANI PALA <chintamanipala67@gmail.com>
2023-11-30 17:52:56 +0530

    Create aside.js

---
+++ assets/js/aside.js
@@ -0,0 +1,14 @@
+// Function to open specific tab
+function openTab(evt, tabName) {
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
