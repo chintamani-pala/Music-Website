a245d04d5075ccd281b6efeff15a285939a2c773
CHINTAMANI PALA <chintamanipala67@gmail.com>
2023-11-30 17:53:11 +0530

    Create contact.js

---
+++ assets/js/contact.js
@@ -0,0 +1,5 @@
+let contactForm=document.getElementById("contactForm")
+contactForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    topRightSmallToast(`Not Implemented Yet`,`error`)
+})
