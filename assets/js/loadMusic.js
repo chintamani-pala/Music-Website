eb9c032c019e0f019e23a8d30a9cf7ee1530789d
CHINTAMANI PALA <chintamanipala67@gmail.com>
2023-12-06 18:57:56 +0530

    Update loadMusic.js

--- assets/js/loadMusic.js
+++ assets/js/loadMusic.js
@@ -1,4 +1,21 @@
 $(document).ready(async function () {
+    const playSong = async (audioPlayer, mediaUrl) => {
+        try {
+            console.log(audioPlayer);
+            console.log(mediaUrl);
+            const response = await fetch(mediaUrl, { method: 'GET', mode: 'cors' });
+            console.log(response);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const mediaBuffer = await response.arrayBuffer();
+            console.log(URL.createObjectURL(new Blob([mediaBuffer])));
+            audioPlayer.src = URL.createObjectURL(new Blob([mediaBuffer]));
+            audioPlayer.play();
+        } catch (error) {
+            console.error('Error while playing song:', error);
+        }
+    };
     document.getElementById('searchForm').addEventListener('submit', function (event) {
         event.preventDefault(); // Prevent the default form submission behavior
     });
@@ -78,8 +95,9 @@ $(document).ready(async function () {
                 const dataIndex = dataTable.row(this).index();
                 const fullData = finalFullData[dataIndex];
                 let audioPlayer = document.getElementById("audioPlayer");
-                audioPlayer.src = fullData.media_url;
-                audioPlayer.play();
+                //audioPlayer.src = fullData.media_url;
+                //audioPlayer.play();
+                playSong(audioPlayer, fullData.media_url);
             }
         });
         searchBtn.innerHTML = "Search";
